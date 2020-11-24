ea03fbbe990b156c99066d945cac23df1de72257
Marcos Costa <64797599+marcos-vcg@users.noreply.github.com>
2020-11-24 11:38:51 -0300

    attScriptSituacaoLocacao

--- WebContent/js/locacao.js
+++ WebContent/js/locacao.js
@@ -3,16 +3,25 @@
 function situacao(td){
 	//Pega elemento Pai duas vezes para pegar a linha atual
 	var selectedRow = td.parentElement.parentElement;
-    
-	var prazo = 7;
-	var locacao = new Date(selectedRow.cells[2].innerHTML);
-	var devolucao = new Date(selectedRow.cells[3].innerHTML);
+	
+	// Pega o campo de Devolução para testar se já foi devolvido
+	var devolucao = selectedRow.cells[3].innerHTML;
+
+	// Pega a data de Hoje e a data de locação
 	var hoje = new Date();
+	var locacao = new Date(selectedRow.cells[2].innerHTML);
+
+	// Passa para milissegundos e subtrai as duas arredondando para baixo para saber os dias completos passados
 	var diff = Math.abs(hoje.getTime() - locacao.getTime());
-	var diasPassados = Math.ceil(diff / (1000 * 60 * 60 * 24));
-	var situacao = "No Prazo";
+	var diasPassados = Math.floor(diff / (1000 * 60 * 60 * 24)); 
+
+	// Define o prazo de 7 dias
+	var prazo = 7;
+
+	// Situação recebe o valor para passar para a tabela via javascript
+	var situacao;
 	
-	
+	// Define a situação de acordo com a devolução ou dias passados
 	if(devolucao != ""){
 		situacao = "Devolvido";
 	} else if (diasPassados > prazo){
@@ -21,10 +30,6 @@ function situacao(td){
 		situacao = "No Prazo";
 	}
 	
-	
 	// Seta o campo situação da linha atual
  	selectedRow.cells[4].innerHTML = situacao;
-
-	//Pega os elementos da linha da tabela a serem editados e preenche os campos
-	document.getElementById("idDep").value = selectedRow.cells[4].innerHTML;
 }
